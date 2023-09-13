ecca58ecdb28e885c1d71e85e1e8b8bf6cc50134
GABRIEL DE SOUZA FURTADO <121953504+gabrielsousaf@users.noreply.github.com>
2023-09-13 20:34:50 -0300

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore"
 const firebaseConfig = {
   apiKey: "AIzaSyDMuJ7DBoFPyO6FxCJB-awO5fq7OlfE-kw",
   authDomain: "miniblog-react-c4eea.firebaseapp.com",
+  databaseURL: "https://miniblog-react-c4eea-default-rtdb.firebaseio.com",
   projectId: "miniblog-react-c4eea",
   storageBucket: "miniblog-react-c4eea.appspot.com",
   messagingSenderId: "246687818862",
-  appId: "1:246687818862:web:b0dbac274186695828b27f"
+  appId: "1:246687818862:web:02d56048dfb5178e28b27f"
 };
-
 const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
