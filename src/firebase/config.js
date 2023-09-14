01d1bba76fa8ddfb4779807938d4803522f4c022
GABRIEL DE SOUZA FURTADO <121953504+gabrielsousaf@users.noreply.github.com>
2023-09-14 12:40:06 -0300

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,13 +3,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBJDcaD_2D5PCyXHud6JUZreHDl-yvEqP8",
-  authDomain: "blog-d6852.firebaseapp.com",
-  projectId: "blog-d6852",
-  storageBucket: "blog-d6852.appspot.com",
-  messagingSenderId: "933806412636",
-  appId: "1:933806412636:web:f0a978e16c97bbe9b76799"
-};
+  apiKey: "AIzaSyA6521ViC8ngf4Zh4G3RBLaF3l5KkTKkY8",
+  authDomain: "mini-blog-react-bd56c.firebaseapp.com",
+  projectId: "mini-blog-react-bd56c",
+  storageBucket: "mini-blog-react-bd56c.appspot.com",
+  messagingSenderId: "444107994884",
+  appId: "1:444107994884:web:67d8867a6bcdf3bbbec251"
+}; 
+
 const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
